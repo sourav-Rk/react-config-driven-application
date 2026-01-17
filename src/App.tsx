96bde2690d5050db0eacd6f96edcf78615c58361
SOURAV-RK <souravrk25@gmail.com>
2026-01-17 19:01:08 +0530

    chore: remove comments

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { useAppConfig } from './hooks/useAppConfig';
-import { Navigation } from './components/layout/Navigation';
-import { PageRenderer } from './components/pages/PageRenderer';
-import { useTheme } from './hooks/useTheme';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { useAppConfig } from "./hooks/useAppConfig";
+import { Navigation } from "./components/layout/Navigation";
+import { PageRenderer } from "./components/pages/PageRenderer";
+import { useTheme } from "./hooks/useTheme";
 
 function App() {
   const config = useAppConfig();
-  useTheme(); 
+  useTheme();
 
   return (
     <Router>
