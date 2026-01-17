32d1ec142ad0cbfaab0f43c014231eada574aba2
SOURAV-RK <souravrk25@gmail.com>
2026-01-17 19:01:56 +0530

    feat(theme): wrap app with ThemeProvider

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,19 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import './index.css';
-import App from './App.tsx';
-import { AppConfigProvider } from './context/AppConfigContext';
-import { ProductFiltersProvider } from './context/ProductFiltersContext';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+import { AppConfigProvider } from "./context/AppConfigContext";
+import { ProductFiltersProvider } from "./context/ProductFiltersContext";
+import { ThemeProvider } from "./context/ThemeContext.tsx";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <AppConfigProvider>
-      <ProductFiltersProvider>
-        <App />
-      </ProductFiltersProvider>
+      <ThemeProvider>
+        <ProductFiltersProvider>
+          <App />
+        </ProductFiltersProvider>
+      </ThemeProvider>
     </AppConfigProvider>
-  </StrictMode>,
+  </StrictMode>
 );
